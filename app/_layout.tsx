5d5f2aedca60a73d25d4974f0a09fb4ba543f86f
ricardobravo92 <ricardojbn5@gmail.com>
2025-10-23 22:13:27 -0400

    index tb

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <Stack>
-        <Stack.Screen name="welcome" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       </Stack>
